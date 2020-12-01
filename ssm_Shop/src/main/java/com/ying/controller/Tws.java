3ecf26638f6c7f07a71e96162c1d3b19944adcd8
张浩 <1627545091@qq.com>
2020-12-01 20:59:20 +0800

    第一次提交(商品展示页面:查询所有页面)

---
+++ ssm_Shop/src/main/java/com/ying/controller/Tws.java
@@ -0,0 +1,12 @@
+package com.ying.controller;
+
+/**
+ * @author ZhangHao(nice)
+ * @create 2020-12-01 20:58
+ */
+public class Tws {
+    public static void main(String[] args) {
+
+        System.out.println(2);
+    }
+}
